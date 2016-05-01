6f2c4b4d5850333d81dfefbd16ccd0f595b8c30d
kauedb <kauedb@gmail.com>
2016-05-01 11:15:08 -0300

    project setup

---
+++ src/main/java/br/com/kauedb/mars_explorer/MarsExplorerApplication.java
@@ -0,0 +1,12 @@
+package br.com.kauedb.mars_explorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarsExplorerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarsExplorerApplication.class, args);
+    }
+}
