ca2b448aab1ff1273b96c91f4c0b1adabdf87adb
kauedb <kauedb@gmail.com>
2016-05-06 14:30:36 -0300

    should command probe refactoring - test fixed

--- src/test/java/br/com/kauedb/mars_explorer/application/ProbeApplicationCommandProbeTest.java
+++ src/test/java/br/com/kauedb/mars_explorer/application/ProbeApplicationCommandProbeTest.java
@@ -1,8 +1,11 @@
 package br.com.kauedb.mars_explorer.application;
 
+import br.com.kauedb.mars_explorer.infrastructure.repository.ProbeRepository;
+import mockit.Injectable;
 import mockit.Tested;
 import org.testng.annotations.Test;
 
+import static mockit.Deencapsulation.setField;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
@@ -14,8 +17,13 @@ public class ProbeApplicationCommandProbeTest {
     @Tested
     private ProbeApplication probeApplication;
 
+    @Injectable
+    private ProbeRepository probeRepository;
+
     @Test
     public void shouldCommandProbe() throws Throwable {
+        setField(probeApplication, "probeRepository", new ProbeRepository());
+
         final ProbeApplication.Command command = ProbeApplication.Command.builder()
                 .upperLimitX(5).upperLimitY(5)
                 .x(1).y(1).direction("N")
